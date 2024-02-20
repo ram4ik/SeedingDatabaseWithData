4ae100164c9198ffb18ac3f8e0a58073b4ec7133
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-20 20:29:55 +0200

    Initial Commit

---
+++ SeedingDatabaseWithData/SeedingDatabaseWithDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeedingDatabaseWithDataApp.swift
+//  SeedingDatabaseWithData
+//
+//  Created by test on 20.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SeedingDatabaseWithDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
